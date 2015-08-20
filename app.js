5fba035a43dac3c06de685e3b9d2f1679f851859
Andrew Cer <cer.andrew@gmail.com>
2015-08-19 20:08:23 -0600

    major overhaul to styling as well as like and dislike functionality

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 var app = angular.module('redditClone', []);
 
-app.controller('subAndSearch', function () {
-
+app.controller('subAndSearch', function ($scope) {
+  $scope.testing = 'hey';
+  $scope.posts = [{
+    imgUrl: 'http://www.heartsandmindsbooks.com/Hiking-Shoes-588x391.jpg',
+    title: 'Trees Man',
+    likeCount: 0,
+    author: 'Bob',
+    description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
+    created: 'date goes here'
+  },
+  {
+    imgUrl: 'http://www.carolinacremation.com/versionone/wp-content/uploads/2011/06/the-great-outdoors-wallpapers_3680_16001.jpg',
+    title: 'Holy Cow!',
+    likeCount: 0,
+    author: 'Mountain Man Jack',
+    description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
+    created: Date()
+  },
+  {
+    imgUrl: 'https://oneadamtwelv.files.wordpress.com/2012/07/p8131124.jpg',
+    title: 'Marshy Marsh',
+    likeCount: 0,
+    author: 'Marsh Man',
+    description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
+    created: Date()
+  }];
+  $scope.upVote = function (index) {
+    $scope.posts[index].likeCount = $scope.posts[index].likeCount + 1
+  }
+  $scope.downVote = function (index) {
+    $scope.posts[index].likeCount = $scope.posts[index].likeCount - 1
+  }
 });
