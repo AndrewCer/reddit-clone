08cbf95665723713a55d18b027ca5ea7d3891f56
Andrew Cer <cer.andrew@gmail.com>
2015-08-20 14:18:06 -0600

    added angular moment

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var app = angular.module('redditClone', []);
+var app = angular.module('redditClone', ['angularMoment']);
 
 app.controller('subAndSearch', function ($scope) {
+  $scope.message = {
+   text: 'hello world!',
+   time: new Date()
+};
   $scope.posts = [{
     imgUrl: 'http://www.heartsandmindsbooks.com/Hiking-Shoes-588x391.jpg',
     title: 'Trees Man',
     likeCount: 2,
     author: 'Bob',
     description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
-    created: 'date goes here'
+    created: new Date()
   },
   {
     imgUrl: 'http://www.carolinacremation.com/versionone/wp-content/uploads/2011/06/the-great-outdoors-wallpapers_3680_16001.jpg',
@@ -15,7 +19,7 @@ app.controller('subAndSearch', function ($scope) {
     likeCount: -2,
     author: 'Mountain Man Jack',
     description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
-    created: Date()
+    created: new Date()
   },
   {
     imgUrl: 'https://oneadamtwelv.files.wordpress.com/2012/07/p8131124.jpg',
@@ -23,7 +27,7 @@ app.controller('subAndSearch', function ($scope) {
     likeCount: 4,
     author: 'Marsh Man',
     description: "Umami vegan disrupt, butcher flannel fingerstache keytar normcore tousled before they sold out. 90's squid fanny pack, biodiesel chillwave fixie retro drinking vinegar before they sold out scenester. Fingerstache food truck cliche migas viral iPhone. Street art authentic dreamcatcher mlkshk Pinterest, small batch narwhal fap 3 wolf moon locavore",
-    created: Date()
+    created: new Date()
   }];
   $scope.upVote = function (index) {
     console.log(this.post);
@@ -57,7 +61,7 @@ app.controller('subAndSearch', function ($scope) {
       $scope.posts.push(schemaObj)
     }
     else {
-      
+
     }
   }
 });
